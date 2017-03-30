78b63c8155d8f46652960a6237edc26ecf84cc3c
Tommy-Lim <tlim24@gmail.com>
2017-03-29 18:42:15 -0700

    added contact, about and index routeS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'main#index'
+
+  get 'about' => 'main#about'
+
+  get 'contact', to: 'main#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
