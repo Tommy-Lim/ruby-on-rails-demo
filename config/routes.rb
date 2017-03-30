7e6eb7599451f7e3c94dbe84f1a3dd43302feabb
Tommy-Lim <tlim24@gmail.com>
2017-03-29 18:52:32 -0700

    added dogs route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'welcome/index'
-
   root 'main#index'
 
   get 'about' => 'main#about'
 
+  get 'welcome' => 'main#welcome'
+
   get 'contact', to: 'main#contact'
+
+  get 'dogs', to: 'main#dogs'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
